a0e41dfad7e3d7a7161aada5907ca5b04aee5fc0
Kickdapie <dannynuyen12345@gmail.com>
2025-06-13 17:27:40 -0500

    added notes to input

--- 03_ui_streamlit/07_chat_elements/chat_message.py
+++ 03_ui_streamlit/07_chat_elements/chat_message.py
@@ -3,9 +3,9 @@
 import streamlit as st
 import numpy as np
 
-message = st.chat_message("assistant")
+message = st.chat_message("assistant") #This is the chat talking and giving data to user
 message.write("Hello human")
 message.bar_chart(np.random.randn(30, 3))
 
-message1 = st.chat_message("user")
+message1 = st.chat_message("user") #This is the user talking
 message1.write("Thanks")
