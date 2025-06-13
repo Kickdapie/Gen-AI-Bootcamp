b1d41b2ba2d65b10eedbde38c601214ef124ccbc
Kickdapie <dannynuyen12345@gmail.com>
2025-06-13 17:44:39 -0500

    added notes to native

--- 03_ui_streamlit/09_secrets_management_native/native.py
+++ 03_ui_streamlit/09_secrets_management_native/native.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 # Everything is accessible via the st.secrets dict:
 
-st.write("DB username:", st.secrets["db_username"])
+st.write("DB username:", st.secrets["db_username"]) #set db_username and db_password in secrets.toml file in .streamlit folder
 st.write("DB password:", st.secrets["db_password"])
 
 # And the root-level secrets are also accessible as environment variables:
@@ -12,4 +12,4 @@ import os
 st.write(
     "Has environment variables been set:",
     os.environ["db_username"] == st.secrets["db_username"],
-)
+) # this checks if os usernmane = secrets username. Use export db_username="" as command for setting username
