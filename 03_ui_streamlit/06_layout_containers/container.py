971e4106b96802661b3eb8fb8958fbbf1fc6f6b6
Kickdapie <dannynuyen12345@gmail.com>
2025-06-13 16:58:31 -0500

    added text to container

--- 03_ui_streamlit/06_layout_containers/container.py
+++ 03_ui_streamlit/06_layout_containers/container.py
@@ -4,10 +4,13 @@
 import streamlit as st
 import numpy as np
 
+st.write("This is outside the container")
+
 with st.container():
    st.write("This is inside the container")
 
    # You can call any Streamlit command, including custom components:
    st.bar_chart(np.random.randn(50, 3))
+   st.write("This is also inside the container")
 
-st.write("This is outside the container")
+st.write("This is also outside the container")
